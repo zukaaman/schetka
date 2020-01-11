40bb631aff8392933957f96359543b8e2973b751
zukaaman@gmail.com <zukaaman@gmail.com>
2020-01-11 21:27:29 +0400

    alldone

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,10 +1,24 @@
 // for header
 
+var nav = document.querySelector(".header__nav");
+var navButton = document.querySelector(".header__nav-button");
+var headerPhone = document.querySelector(".header__phone");
+
+navButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  nav.classList.toggle("header__nav--active");
+  (this).classList.toggle("header__nav-button--active");
+  headerPhone.classList.toggle("header__phone--active");
+})
+
 const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
-    e.preventDefault()
+    e.preventDefault();
+    nav.classList.remove("header__nav--active");
+    headerPhone.classList.remove("header__phone--active");
+    navButton.classList.remove("header__nav-button--active");
 
     const blockID = anchor.getAttribute('href').substr(1)
 
